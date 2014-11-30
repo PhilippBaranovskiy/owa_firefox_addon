7582e01be4f4a2093d64fab4ab77866f7432932c
Mihai Chezan <mihai.chezan@softgress.com>
2014-11-30 21:26:01 +0200

    allow user to change delay between new email checks && code clean ups

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-OWA firefox addon
+Outlook Web App Notifications - firefox extension
 =================
 
-Firefox addon for corporate OWA is useful to show counter of unread messages and notify about it via system notifications
+Firefox extension that adds system notifications capability to OWA (Outlook Web App).
 
-Ready to install assemblies associated with releases.
+You must be logged in OWA in order for this to work. No configuration needed.
+
+How it works: when new email arrives into OWA a system notification about this event will be shown.
+Also the fav icon and document title of the OWA tab will be updated to show the number of unread emails (so this also works with pinned tabs).
+
+Fork of [OWA_firefox_addon by Phil Baranovskiy](https://github.com/rockfield/owa_firefox_addon).
+
+Ready to install assemblies associated with releases (see builds folder).
 
 ![Screenshot](https://raw.githubusercontent.com/rockfield/owa_firefox_addon/master/owa_pub.png "Screenshot")
 
--- data/icon_notifications.js
+++ data/icon_notifications.js
@@ -1,20 +1,20 @@
-var prev_unread_messages = 0;
+var timer;
+var unreadMessages = 0;
+var documentTitle = document.title;
 
-var owa_icon = document.createElement("link");
-owa_icon.rel = "icon";
-owa_icon.type = "image/png";
-owa_icon.sizes = "64x64";
-owa_icon.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVQ4y6WTz0tUURTHP/e9N04MBqNJQRDYDKK5tEAINxHRsk24sWWL6G+wheImWriYaBVuRHMZGrVxIQ4pxRQtZiU5OjNU2nOycV6D8+bNPS7e/MAZfC38woUL55zPvffc71EiIpxDRlDw6NhjdeswEGA1Nrbjkso5fM47bO6USO6WKP+rgQjyciwY8OxDlpn3ebAMMFUrGjbA0/+/gWEoCJtnJi2nC8wm92mgy65mYSJGvC/SekJTIsQvXWDbPm7e5kexytr3IihAC28eDRDvi3Q20VBwMH2LufEYrx72g/Y/SIkGV3Pn+kXKz0dJZoosff19uokAD4Z7SKz/YupdnsKLUZ4uZiBk8GTsKiPXuukOm8RmvrBX9Lg7EO0EWKbCd4WgRQB/JdZ/svTNZmPbgZAByo+0AYS36UNeT9/k3mCU+ZQNYQu0xjIVG9myX1yX6gQoqlromUzR39tFxq74TdRQ9QQqtVaVJyhF5xMANJD5457yw+PbV7h/I9oEiMDQ5UgLUNP1E9qNVFckZDJYL2iXagxT7m+FVK7Ep6zDx90Sm1kH7WrfyomzrYwEaO+oIivpQlCKqPOO8wknR+1GRWhuAQAAAABJRU5ErkJggg==";
-document.head.appendChild(owa_icon);
+var owaIcon = document.createElement("link");
+owaIcon.rel = "icon";
+owaIcon.type = "image/png";
+owaIcon.sizes = "64x64";
+owaIcon.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVQ4y6WTz0tUURTHP/e9N04MBqNJQRDYDKK5tEAINxHRsk24sWWL6G+wheImWriYaBVuRHMZGrVxIQ4pxRQtZiU5OjNU2nOycV6D8+bNPS7e/MAZfC38woUL55zPvffc71EiIpxDRlDw6NhjdeswEGA1Nrbjkso5fM47bO6USO6WKP+rgQjyciwY8OxDlpn3ebAMMFUrGjbA0/+/gWEoCJtnJi2nC8wm92mgy65mYSJGvC/SekJTIsQvXWDbPm7e5kexytr3IihAC28eDRDvi3Q20VBwMH2LufEYrx72g/Y/SIkGV3Pn+kXKz0dJZoosff19uokAD4Z7SKz/YupdnsKLUZ4uZiBk8GTsKiPXuukOm8RmvrBX9Lg7EO0EWKbCd4WgRQB/JdZ/svTNZmPbgZAByo+0AYS36UNeT9/k3mCU+ZQNYQu0xjIVG9myX1yX6gQoqlromUzR39tFxq74TdRQ9QQqtVaVJyhF5xMANJD5457yw+PbV7h/I9oEiMDQ5UgLUNP1E9qNVFckZDJYL2iXagxT7m+FVK7Ep6zDx90Sm1kH7WrfyomzrYwEaO+oIivpQlCKqPOO8wknR+1GRWhuAQAAAABJRU5ErkJggg==";
+document.head.appendChild(owaIcon);
 
 var img = document.createElement("img");
-img.src = owa_icon.href;
+img.src = owaIcon.href;
 
-var current_icon_number = 0;
-
-function generate_icon(number, clear) {
+function generateIcon(number, clear) {
   number = new String(number);
-  
+
   var canvas = document.createElement("canvas");
   canvas.width = img.width;
   canvas.height = img.height;
@@ -40,82 +40,103 @@ function generate_icon(number, clear) {
       context.quadraticCurveTo(x, y, x+radius, y);
       context.fill();
     })(ctx, 2, -2, 16, 14, 0);
-  
+
     ctx.font = "bold 10px Arial";
     ctx.textBaseline = "top";
     ctx.textAlign = "center";
     ctx.fillStyle = "white";
     ctx.fillText(number, 9,2);
   }
-  
+
   return canvas.toDataURL("image/png");
 }
 
-function get_base64_icon(number) {
-  
-  if (!number)
+function getBase64Icon(number) {
+  if (!number) {
     number = 0;
-  else if (number > 99)
+  } else if (number > 99) {
     number = "99";
+  }
   var icon;
-  
-  if (number != 0)
-    icon = generate_icon(number);
-  else
-    icon = generate_icon(number, true);
-        
-  current_icon_number = number;
-  
+  if (number != 0) {
+    icon = generateIcon(number);
+  } else {
+    icon = generateIcon(number, true);
+  }
   return icon;
 }
 
-function set_favicon(count) {
-  var icon = get_base64_icon(count);
+function setFavicon(count) {
+  var icon = getBase64Icon(count);
   var s = document.querySelectorAll("link[rel*='icon'][type='image/png']");
 
   if (s.length != 1 || s[0].href != "data:image/png;base64,"+icon) {
     for(var i = s.length-1; i >= 0; i--){
       s[i].remove();
     }
-    owa_icon.href = icon;
-    document.head.appendChild(owa_icon);
+    owaIcon.href = icon;
+    document.head.appendChild(owaIcon);
   }
 }
 
-function countIt(unread_container) {
+function setDocumentTitle(count) {
+  var countPrefix = "";
+  if (count > 0) {
+    countPrefix = "(" + count + ") ";
+  }
+  document.title = countPrefix + documentTitle;
+}
+
+function countIt(unreadContainer) {
   var count = 0;
-  for(var u_node = unread_container.length-1; u_node>=0; u_node--) {
-    count += parseInt(unread_container[u_node].innerHTML.match(/\d/gi).join(""), 10);
+  for(var u_node = unreadContainer.length-1; u_node>=0; u_node--) {
+    count += parseInt(unreadContainer[u_node].innerHTML.match(/\d/gi).join(""), 10);
   }
   return count;
 }
-  
+
 function countUnreadMessages() {
-  var unread_container;
+  var unreadContainer;
   var count = 0;
 
   var folder_panes = document.querySelectorAll("[aria-label='Folder Pane']");
   if (folder_panes.length > 0) {
     for(var pane = folder_panes.length-1; pane >= 0; pane--){
-      unread_container = folder_panes[pane].querySelectorAll("[id*='.ucount']");
-      count = countIt(unread_container);
+      unreadContainer = folder_panes[pane].querySelectorAll("[id*='.ucount']");
+      count = countIt(unreadContainer);
     }
   } else {
-    unread_container = document.querySelectorAll('#spnCV');
-    count = countIt(unread_container);
+    unreadContainer = document.querySelectorAll('#spnCV');
+    count = countIt(unreadContainer);
   }
   return count;
 }
 
 function notify() {
   var count = countUnreadMessages();
-  if (count != prev_unread_messages) {
-	set_favicon(count);
-	if (count > prev_unread_messages) {
-	  self.port.emit("notify", count - prev_unread_messages);
+  if (count != unreadMessages) {
+    setFavicon(count);
+    setDocumentTitle(count);
+    if (count > unreadMessages) {
+      self.port.emit("notify", count - unreadMessages);
     }
   }
-  prev_unread_messages = count;
+  unreadMessages = count;
 }
 
-setInterval(notify, 1000);
+self.port.on("startMonitor", function(delayBetweenChecks) {
+  if (timer) {
+    clearInterval(timer);
+  }
+  if (delayBetweenChecks < 1) {
+    delayBetweenChecks = 1;
+  }
+  timer = setInterval(notify, delayBetweenChecks * 1000);
+});
+
+self.port.on("detach", function() {
+  clearInterval(timer);
+  setFavicon(0);
+  setDocumentTitle(0);
+});
+
