2e91b34a447b54117d05078fbcf39ad2431922ec
rockfield <me@philipp.ninja>
2015-02-13 18:47:33 +0300

    cleaned not used code

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,14 @@
 var main = require("./main");
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
+// exports["test main"] = function(assert) {
+// 	if ( Number.isInteger(countUnreadMessages()) ) {
+// 		assert.pass("Unit test running!");
+// 	}
+// };
 
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
-};
+// exports["test main async"] = function(assert, done) {
+//   assert.pass("async Unit test running!");
+//   done();
+// };
 
 require("sdk/test").run(exports);
